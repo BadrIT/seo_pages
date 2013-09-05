41292fef440b491c90404dd738bc1f3f62984e86
Hassan <thepythonic@gmail.com>
2013-09-05 11:59:18 +0200

    automatically load helper, no need to load it from rails app.

--- lib/seo/engine.rb
+++ lib/seo/engine.rb
@@ -1,5 +1,10 @@
 module Seo
   class Engine < ::Rails::Engine
     isolate_namespace Seo
+    initializer 'seo.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        helper Seo::Engine.helpers
+      end
+    end
   end
 end
