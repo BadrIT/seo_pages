03e3c1e7ca367918023052e2e2e4f8a69bd9e208
Hassan <thepythonic@gmail.com>
2013-09-05 12:12:34 +0200

    fix getting default seo_data

--- app/helpers/seo/application_helper.rb
+++ app/helpers/seo/application_helper.rb
@@ -3,12 +3,12 @@ module Seo
 
     def seo_data
       @seo_default ||= Seo::SeoPageDefault.first
-      unless @seo = Seo::SeoPage.where(url_identifier: request.fullpath).first
-        seo.page_title ||= @seo_default.page_title
-        seo.meta_keywords ||= @seo_default.meta_keywords
-        seo.meta_description ||= @seo_default.meta_description
-      end
-
+      @seo = Seo::SeoPage.where(url_identifier: request.fullpath).first || Seo::SeoPage.new
+      
+      @seo.page_title ||= @seo_default.page_title
+      @seo.meta_keywords ||= @seo_default.meta_keywords
+      @seo.meta_description ||= @seo_default.meta_description
+      
       @seo
     end
 
