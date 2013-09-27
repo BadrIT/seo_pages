b745f2dc4e1eb2175159cdd19477b2e6a580a518
Hassan <thepythonic@gmail.com>
2013-09-27 22:34:28 +0200

    memoize the regex, no need to create it each time

--- app/models/seo_pages/seo_page.rb
+++ app/models/seo_pages/seo_page.rb
@@ -5,21 +5,27 @@ module SeoPages
     scope :regex, -> { where(is_regex: true)}
     scope :non_regex, -> { where(is_regex: false)}
 
+    @@regex = {}
+
     def self.find_by_url_identifier url
-      seo = direct_match url
+      seo = exact_match url
       unless seo
         seo = regex_match url
       end
     end
 
-    def self.direct_match url
+    def self.exact_match url
       non_regex.where(url_identifier: url).first
     end
 
     def self.regex_match url
       regex.to_a.find do |seo|
-        (Regexp.new(seo.url_identifier) =~ url) != nil
+        (get_regex(seo.url_identifier) =~ url) != nil
       end
+    end
+
+    def self.get_regex url
+      @@regex[url].nil? ? @@regex[url] = Regexp.new(url) : @@regex[url]
     end
   end
 end
